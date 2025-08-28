7f9a80712b4f61488a07bb1d94d79868a2274c63
jccdev45 <jccdev45@gmail.com>
2025-08-28 19:28:12 -0400

    Add limit filter

--- src/components/map-filters.tsx
+++ src/components/map-filters.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Link, useNavigate, useSearch } from "@tanstack/react-router";
+import { CircleQuestionMark } from "lucide-react";
 import { useState } from "react";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
 
 const boroughData = {
 	Manhattan: { latitude: 40.776676, longitude: -73.971321, zoom: 12 },
@@ -64,6 +73,75 @@ export function MapFilters() {
 						All
 					</Link>
 				</div>
+			</div>
+			<div className="flex items-center gap-2">
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger>
+							<CircleQuestionMark className="size-4" />
+						</TooltipTrigger>
+						<TooltipContent>
+							<div className="text-balance">
+								<p>
+									The limit only applies to the raw data returned by the API.
+								</p>
+								<p>
+									The actual results are smaller due to the sanitization applied
+									to that raw data.
+								</p>
+								<p className="text-destructive-foreground w-fit bg-destructive-foreground/50 p-1 font-bold">
+									NOTE: higher limits may cause slower initial load times.
+								</p>
+							</div>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+				<span>Limit:</span>
+				<RadioGroup
+					defaultValue={searchParams?.$limit?.toString() || "1000"}
+					className="flex items-center gap-2"
+				>
+					<div className="flex items-center gap-1">
+						<Link
+							className="flex items-center space-x-2"
+							to="/map"
+							search={(prev) => ({ ...prev, $limit: 1000 })}
+						>
+							<RadioGroupItem value="1000" id="1000" />
+						</Link>
+						<Label htmlFor="1000">1000</Label>
+					</div>
+					<div className="flex items-center gap-1">
+						<Link
+							className="flex items-center space-x-2"
+							to="/map"
+							search={(prev) => ({ ...prev, $limit: 2000 })}
+						>
+							<RadioGroupItem value="2000" id="2000" />
+						</Link>
+						<Label htmlFor="2000">2000</Label>
+					</div>
+					<div className="flex items-center gap-1">
+						<Link
+							className="flex items-center space-x-2"
+							to="/map"
+							search={(prev) => ({ ...prev, $limit: 3000 })}
+						>
+							<RadioGroupItem value="3000" id="3000" />
+						</Link>
+						<Label htmlFor="3000">3000</Label>
+					</div>
+					<div className="flex items-center gap-1">
+						<Link
+							className="flex items-center space-x-2"
+							to="/map"
+							search={(prev) => ({ ...prev, $limit: 5000 })}
+						>
+							<RadioGroupItem value="5000" id="5000" />
+						</Link>
+						<Label htmlFor="5000">5000</Label>
+					</div>
+				</RadioGroup>
 			</div>
 			<div className="flex items-center gap-2">
 				<span>Borough:</span>

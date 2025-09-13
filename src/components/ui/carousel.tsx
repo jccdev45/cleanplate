6516e740af4ad4667c8585fcf48c048215833134
jccdev45 <jccdev45@gmail.com>
2025-09-13 15:49:53 -0400

    feat(carousel): add mounted state to control button disabled state

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -177,6 +177,10 @@ function CarouselPrevious({
 	...props
 }: React.ComponentProps<typeof Button>) {
 	const { orientation, scrollPrev, canScrollPrev } = useCarousel();
+	const [mounted, setMounted] = React.useState(false);
+	React.useEffect(() => {
+		setMounted(true);
+	}, []);
 
 	return (
 		<Button
@@ -190,7 +194,7 @@ function CarouselPrevious({
 					: "-top-12 left-1/2 -translate-x-1/2 rotate-90",
 				className,
 			)}
-			disabled={!canScrollPrev}
+			disabled={mounted ? !canScrollPrev : undefined}
 			onClick={scrollPrev}
 			{...props}
 		>
@@ -207,6 +211,10 @@ function CarouselNext({
 	...props
 }: React.ComponentProps<typeof Button>) {
 	const { orientation, scrollNext, canScrollNext } = useCarousel();
+	const [mounted, setMounted] = React.useState(false);
+	React.useEffect(() => {
+		setMounted(true);
+	}, []);
 
 	return (
 		<Button
@@ -220,7 +228,7 @@ function CarouselNext({
 					: "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
 				className,
 			)}
-			disabled={!canScrollNext}
+			disabled={mounted ? !canScrollNext : undefined}
 			onClick={scrollNext}
 			{...props}
 		>
